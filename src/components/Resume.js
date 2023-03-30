cf7697454b143e4a38f8c7f97574cec4ce37f9ea
SaToGo <stoledano97@gmail.com>
2023-03-30 21:21:38 +0200

    add grid to give format to the Resume at Resume.css

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -11,7 +11,10 @@ class Resume extends Component {
         return(
             <div className='Resume'>
                 <div className='Resume__body'>
-
+                    <div className='Resume__border'> border </div>
+                    <div className='Resume__personalInformation'> Personal information </div>
+                    <div className='Resume__Education'> Education </div>
+                    <div className='Resume__Experience'> Experience </div>
                 </div>
             </div>
         )

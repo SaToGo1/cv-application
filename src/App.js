346d40ae445bfbf632746c5019f96be7d3b9fda7
SaToGo <stoledano97@gmail.com>
2023-03-30 19:30:01 +0200

    React boilerplate

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+  }
+
+  render(){
+
+    return(
+      <div>
+        Init
+      </div>
+    )
+  }
+}
+
+export default App;

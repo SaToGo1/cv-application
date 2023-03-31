d12374776ca085ea16ce6c5aad231c5f6ef67cbc
SaToGo <stoledano97@gmail.com>
2023-03-31 14:49:45 +0200

    Add personalInformation component content and style
    Added 2 icons with format SVG

--- src/components/PersonalInformation.js
+++ src/components/PersonalInformation.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import '../styles/PersonalInformation.css'
+import atIcon from '../img/at.svg'
+import phoneIcon from '../img/phone.svg'
 
 class PersonalInformation extends Component{
     constructor(){
@@ -9,7 +11,27 @@ class PersonalInformation extends Component{
     render(){
         return(
             <div className='PersonalInformation'>
-                Hello
+                <div className='PersonalInformation__rightSide'>
+                    <div className='PersonalInformation__NameDiv'>
+                        <p className='PersonalInformation__name'>Name</p>
+                        <p className='PersonalInformation__name'>Surname</p>
+                    </div>
+                    <div>
+                        <p className='PersonalInformation__title'>Title</p>
+                    </div>
+                </div>
+                <div className='PersonalInformation__leftSide'>
+                    <div className='PersonalInformation__iconData'>
+                        <img src={atIcon} alt='e-mail' className='PersonalInformation__icon' />
+                        <p>E-mail</p>
+                    </div>
+                    <div className='PersonalInformation__iconData'>
+                        <img src={phoneIcon} alt='phone' className='PersonalInformation__icon' />
+                        <p>Telephone</p>
+                    </div>
+                </div>
+
+                <hr className="rounded"></hr>
             </div>
         )
     }

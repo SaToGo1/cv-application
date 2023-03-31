d01b7bb48c191f8940ca0630b55696dac2a9b505
SaToGo <stoledano97@gmail.com>
2023-03-31 12:25:29 +0200

    add PersonalInformation component and style

---
+++ src/components/PersonalInformation.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import '../styles/PersonalInformation.css'
+
+class PersonalInformation extends Component{
+    constructor(){
+        super()
+    }
+
+    render(){
+        return(
+            <div className='PersonalInformation'>
+                Hello
+            </div>
+        )
+    }
+}
+
+export default PersonalInformation;
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import '../styles/Resume.css'
+import PersonalInformation from './PersonalInformation'
 
 class Resume extends Component {
     constructor(){
@@ -12,7 +13,9 @@ class Resume extends Component {
             <div className='Resume'>
                 <div className='Resume__body'>
                     <div className='Resume__border'> border </div>
-                    <div className='Resume__personalInformation'> Personal information </div>
+                    <div className='Resume__personalInformation'> 
+                        <PersonalInformation />
+                    </div>
                     <div className='Resume__Education'> Education </div>
                     <div className='Resume__Experience'> Experience </div>
                 </div>

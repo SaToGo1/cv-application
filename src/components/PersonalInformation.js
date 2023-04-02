3e3be111bc6804b25ad8b72bde705c8fcd3dd79c
SaToGo <stoledano97@gmail.com>
2023-04-02 20:50:01 +0200

    Add Functionality to change the name surname and title,
    when we click over them it will show an input we can use it
    to give a new value to these fields, and if we press enter or
    change the focus of the input, the input will disappear and we
    will se name surname and title with the new value.

--- src/components/PersonalInformation.js
+++ src/components/PersonalInformation.js
@@ -6,28 +6,185 @@ import phoneIcon from '../img/phone.svg'
 class PersonalInformation extends Component {
     constructor(){
         super()
+
+        this.state = {
+            showNameInput: false,
+            showSurNameInput: false,
+            showTitleInput: false,
+            information: {
+                name: "name",
+                surname: "surname",
+                title: "Title",
+                mail: "mail",
+                telephone: "telephone"
+            },
+        };
+    }
+
+/*  =============== 
+    Name Handlers
+    =============== */
+
+        handleNameClick = () => {
+            this.setState ({
+                showNameInput: true,
+            })
+        }
+
+        handleNameChange = (e) => {
+            const information = this.state.information;
+            this.setState ({
+                information: {
+                    ...information,
+                    name: e.target.value,
+                },
+            })
+        }
+
+/*  =============== 
+    Surname Handlers
+    =============== */
+
+        handleSurNameClick = () => {
+            this.setState ({
+                showSurNameInput: true,
+            })
+        }
+
+        handleSurNameChange = (e) => {
+            const information = this.state.information;
+            this.setState ({
+                information: {
+                    ...information,
+                    surname: e.target.value,
+                },
+            })
+        }
+
+/*  =============== 
+    Title Handlers
+    =============== */
+        handleTitleClick = () => {
+            this.setState ({
+                showTitleInput: true,
+            })
+        }
+
+        handleTitleChange = (e) => {
+            const information = this.state.information;
+            this.setState ({
+                information: {
+                    ...information,
+                    title: e.target.value,
+                },
+            })
+        }
+
+/*  =============== 
+    General Handlers
+    =============== */
+
+    handleKeyDown = (e) => {
+        if(e.key === 'Enter') {
+            this.setState ({
+                showNameInput: false,
+                showSurNameInput: false,
+                showTitleInput: false,
+            })
+        }
+    }
+
+    handleFocusOut = () => {
+        this.setState ({
+            showNameInput: false,
+            showSurNameInput: false,
+            showTitleInput: false,
+        })
     }
 
     render(){
+        const { name, surname, title, mail, telephone } = this.state.information;
+        const { showNameInput, showSurNameInput, showTitleInput } = this.state;
+
         return(
             <div className='PersonalInformation'>
                 <div className='PerInfo__rightSide'>
-                    <div className='PerInfo__NameDiv'>
-                        <p className='PerInfo__name'>Name</p>
-                        <p className='PerInfo__name'>Surname</p>
+                    <div className='PerInfo__NameSurnameDiv'>
+                        
+                        {!showNameInput && (
+                            <p 
+                                className='PerInfo__name'
+                                onClick={this.handleNameClick}
+                            >
+                                {name}
+                            </p>
+                        )}
+
+                        {showNameInput && (
+                            <input 
+                                type='text' 
+                                autoFocus 
+                                className='PerInfo__Input' 
+                                value={name}
+                                onChange={this.handleNameChange}
+                                onBlur={this.handleFocusOut}
+                                onKeyDown={this.handleKeyDown}
+                            />
+                        )}
+                        
+
+                        {!showSurNameInput && (
+                            <p 
+                                className='PerInfo__name'
+                                onClick={this.handleSurNameClick}
+                            >
+                                {surname}
+                            </p>
+                        )}
+
+                        {showSurNameInput && (
+                            <input 
+                                type='text' 
+                                autoFocus 
+                                className='PerInfo__Input' 
+                                value={surname}
+                                onChange={this.handleSurNameChange}
+                                onBlur={this.handleFocusOut}
+                                onKeyDown={this.handleKeyDown}
+                            />
+                        )}
                     </div>
-                    <div>
-                        <p className='PerInfo__title'>Title</p>
+                    <div className='PerInfo__titleDiv'>
+                        {!showTitleInput && (
+                            <p 
+                                className='PerInfo__title'
+                                onClick={this.handleTitleClick}
+                            >
+                                {title}
+                            </p>
+                        )}
+
+                        {showTitleInput && (
+                            <input
+                                type='text' 
+                                autoFocus 
+                                className='PerInfo__Input'
+                                value={title}
+                                onChange={this.handleTitleChange}
+                                onBlur={this.handleFocusOut}
+                                onKeyDown={this.handleKeyDown}
+                            />
+                        )}
                     </div>
                 </div>
                 <div className='PerInfo__leftSide'>
                     <div className='PerInfo__iconData'>
                         <img src={atIcon} alt='e-mail' className='PerInfo__icon' />
-                        <p>E-mail</p>
+                        <p>{mail}</p>
                     </div>
                     <div className='PerInfo__iconData'>
                         <img src={phoneIcon} alt='phone' className='PerInfo__icon' />
-                        <p>Telephone</p>
+                        <p>{telephone}</p>
                     </div>
                 </div>
             </div>
